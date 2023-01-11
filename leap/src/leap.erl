558a3c1a977068c2ea0b2950d1f98f4506994560
Kim Hallberg <hallberg.kim@gmail.com>
2023-01-11 19:18:11 +0100

    Complete `Leap` exercise

--- leap/src/leap.erl
+++ leap/src/leap.erl
@@ -2,5 +2,11 @@
 
 -export([leap_year/1]).
 
-
-leap_year(_Year) -> undefined.
+% https://learnyousomeerlang.com/types-or-lack-thereof#for-type-junkies
+% https://www.erlang.org/doc/reference_manual/typespec.html
+% type bool() is now called boolean(); please use the new name instead.
+-spec leap_year(non_neg_integer()) -> boolean().
+leap_year(_Year) when _Year rem 400 =:= 0 -> true;
+leap_year(_Year) when _Year rem 100 =:= 0 -> false;
+leap_year(_Year) when _Year rem 4 =:= 0 -> true;
+leap_year(_) -> false.
