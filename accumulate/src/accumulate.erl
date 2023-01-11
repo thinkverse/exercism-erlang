09856d72c23cd12a648aab28ed50b1ed68924900
Kim Hallberg <hallberg.kim@gmail.com>
2023-01-11 19:22:33 +0100

    Complete `Accumulate` exercise

--- accumulate/src/accumulate.erl
+++ accumulate/src/accumulate.erl
@@ -4,5 +4,7 @@
 %%
 %% given a fun and a list, apply fun to each list item replacing list item with fun's return value.
 %%
+%% Thank you List Comprehensions!
+%% https://learnyousomeerlang.com/starting-out-for-real#list-comprehensions
 -spec accumulate(fun((A) -> B), list(A)) -> list(B).
-accumulate(_Fn, _List) -> undefined.
+accumulate(_Fn, _List) -> [_Fn(X) || X <- _List].
