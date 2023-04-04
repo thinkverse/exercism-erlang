ea52c1f52033f0178a55dffdd9ad2fe3fc33a9dc
Kim Hallberg <hallberg.kim@gmail.com>
2023-04-04 07:46:23 +0200

    Add `Difference of Squares` exercise

---
+++ difference-of-squares/src/difference_of_squares.erl
@@ -0,0 +1,10 @@
+-module(difference_of_squares).
+
+-export([difference_of_squares/1, square_of_sum/1, sum_of_squares/1]).
+
+
+difference_of_squares(_Number) -> undefined.
+
+square_of_sum(_Number) -> undefined.
+
+sum_of_squares(_Number) -> undefined.
