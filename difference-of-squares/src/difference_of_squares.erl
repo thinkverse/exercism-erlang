fab586350945c79a78573ccaf931774c7e0fcd0a
Kim Hallberg <hallberg.kim@gmail.com>
2023-04-04 08:03:07 +0200

    Complete `Difference of Squares` exercise

--- difference-of-squares/src/difference_of_squares.erl
+++ difference-of-squares/src/difference_of_squares.erl
@@ -3,8 +3,18 @@
 -export([difference_of_squares/1, square_of_sum/1, sum_of_squares/1]).
 
 
-difference_of_squares(_Number) -> undefined.
+difference_of_squares(_Number) ->
+  square_of_sum(_Number) - sum_of_squares(_Number).
 
-square_of_sum(_Number) -> undefined.
+square_of_sum(_Number) ->
+  power_of(lists:sum(sequence_of_ints(_Number)), 2).
 
-sum_of_squares(_Number) -> undefined.
+sum_of_squares(_Number) -> 
+  lists:sum([power_of(N, 2) || N <- sequence_of_ints(_Number)]).
+
+sequence_of_ints(From) ->
+  lists:seq(0, From).
+
+power_of(X, Y) ->
+  % Had do look up example to figure out int from float.
+  erlang:trunc(math:pow(X, Y)).
